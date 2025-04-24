1e98cf3538f471d825ea8fc5996f877a4759b7f7
imtiazhasanBD <imtiazhasan500@gmail.com>
2025-04-25 02:07:58 +0600

    feat: add Product component with styling and functionality

---
+++ app/components/Product.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { FaHeart } from "react-icons/fa";
+
+
+const Product = ({ product }) => {
+    
+    const {
+    id,
+    name,
+    image,
+    short_desc,
+    price,
+    category,
+    discount_amount,
+} = product;
+console.log(product);
+
+  return (
+    <div className="bg-white drop-shadow-sm border p-2 flex flex-col gap-4 border-gray-200 rounded-lg overflow-hidden hover:border-gray-400 duration-200 cursor-pointer">
+      <div className="w-full relative p-2 group">
+        <img
+          className="w-full h-full rounded-md object-cover hover:scale-110 duration-300"
+          src={`https://admin.refabry.com/storage/product/${image}`}
+          alt=""
+        />
+        <p className={`absolute top-0 bg-gray-200 text-sm font-bold  p-1 mt-1`}>
+          save ${discount_amount}
+        </p>
+        <FaHeart className="absolute top-0 right-0 mt-2 mr-2 text-gray-300 hover:text-gray-500 cursor-pointer" />
+      </div>
+      <div className="flex-1">
+        <p className="text-xs uppercase text-gray-400 pb-1">{category.name}</p>
+        <h1 className="text-lg font-bold line-clamp-2">{name}</h1>
+        <p className="desc text-sm hidden md:block lg:block">
+          {short_desc.substring(0, 50) + "..."}
+        </p>
+        <div className="price-buy flex gap-2 items-center pr-2 md:py-1">
+            <p className="line-through text-gray-500 text-sm">${price}</p>
+            <p className="font-bold text-skyText">${price - discount_amount}</p>
+        </div>
+      </div>
+      <button
+        className={`bg-[#f7f7f7] text-black font-semibold p-2 border-[1px] border-gray-200 hover:border-skyText rounded-full text-sm hover:bg-green-700 hover:text-white duration-200 cursor-pointer`}
+      >
+        ADD TO CART
+      </button>
+    </div>
+  );
+};
+
+export default Product;
--- app/components/Products.jsx
+++ app/components/Products.jsx
@@ -1,23 +1,37 @@
-"use client"
+"use client";
 
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchProducts } from '../features/products/productSlice';
-import { useEffect } from 'react';
+import { useDispatch, useSelector } from "react-redux";
+import { fetchProducts } from "../features/products/productSlice";
+import { useEffect } from "react";
+import Product from "./Product";
 
 const Products = () => {
-    const dispatch = useDispatch();
-    const {products} = useSelector((state) => state.product);
-  
-    useEffect(() => {
-      dispatch(fetchProducts());
-    }, []);
+  const dispatch = useDispatch();
+  const { products } = useSelector((state) => state.product);
 
-    
-console.log(products);
+  useEffect(() => {
+    dispatch(fetchProducts());
+  }, []);
   
+  if(!products.data){
+    return "loading..."
+  }
+  console.log(products.data);
+
   return (
-    <div>Products</div>
-  )
-}
+    <div className="md:w-[95%] px-2 m-auto mt-5 mb-4 bg-white">
+    <div className="flex justify-between p-2 px-4 font-semibold border-b-2">
+      <p className="text-xl">Just For You</p>
+      <button className="text-green-700">View All Products</button>
+    </div>
+    <div className="grid grid-cols-2 sm:grid-cols-3 xl:grid-cols-5 md:grid-cols-4 gap-5 md:p-4 z-20 pt-4">
+      {products?.data &&
+        products?.data.map((product) => (
+          <Product product={product} key={product.unique_id} />
+        ))}
+    </div>
+  </div>
+  );
+};
 
-export default Products
+export default Products;
