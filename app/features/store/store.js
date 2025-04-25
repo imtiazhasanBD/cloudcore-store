e205904bedbd787ed522d5324787d6500b3c38f7
imtiazhasanBD <imtiazhasan500@gmail.com>
2025-04-25 23:07:55 +0600

    feat(store): add localStorage integration for state persistence

--- app/features/store/store.js
+++ app/features/store/store.js
@@ -1,12 +1,21 @@
 import { configureStore } from '@reduxjs/toolkit';
 import productReducer from '../products/productSlice';
 import cartReducer from '../cart/cartSlice'
+import { loadState, saveState } from '../localStorage/localStorage';
 
+const persistedState = loadState();
 
 export const store = configureStore({
   reducer: {
     product: productReducer,
     cart: cartReducer
-  }
+  },
+  preloadedState: persistedState
 });
 
+store.subscribe(() => {
+  saveState({
+    product: store.getState().product,
+    cart: store.getState().cart
+  })
+})
